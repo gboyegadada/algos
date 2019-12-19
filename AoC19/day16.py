30855e603aa7b2ca11171da1555fa01591a2899b
Gboyega Dada <boyega@gmail.com>
2019-12-19 16:07:16 +0100

    AoC19 - Day 16

--- AoC19/day16.py
+++ AoC19/day16.py
@@ -1,4 +1,6 @@
 import sys
+from math import ceil
+from itertools import accumulate
 
 
 if len(sys.argv) == 1 or sys.argv[1] == '-v': 
@@ -12,25 +14,63 @@ for l in open(f):
   data = [int(x) for x in l.strip()]
 
 
-def solve_digit(pos: int, digits: list):
-  dl = len(digits)
+def num(l):
+  return sum([ x * 10**i for i, x in enumerate(reversed(l))])
 
-  base_seq = [0]*(pos+1) + [1]*(pos+1) + [0]*(pos+1) + [-1]*(pos+1)
-  seq = base_seq[1:] + base_seq * ((dl // len(base_seq)) + 1)
+def solve_digit(pos: int, digits: list, dl: int):
+  
+  base_seq = [0]*(pos+1) + [1]*(pos+1) + [0]*(pos+1) + [-1]*(pos+1)  
+  seq = base_seq[pos+1:] + (base_seq * ceil(dl // len(base_seq)))
 
 
+  # st = []
   acc = 0
-
-  for d, s in zip(digits, seq[:dl]): 
+  for d, s in zip(digits[pos:], seq[:len(digits)+pos]): 
+    # st.append('{}*{}'.format(d, s))
     acc += d * s
 
+  # print(' + '.join(st), '=', abs(acc) % 10, '\n')
   return abs(acc) % 10
 
 
+'''
+Solution 1
+
+'''
+
+N = len(data)
+d = data[:]
+
 for _ in range(100):
-  data = [ solve_digit(i, data) for i in range(len(data)) ]
+  d = [ solve_digit(i, d, N) for i in range(N) ]
 
-print('Solution 1:', ''.join(map(str, data[:8])))
+print('Solution 1:', num(d[:8]))
 
 
+'''
+Solution 2
 
+
+1st digit -------> no zeros 
+2nd digit -------> 1 zeros | 0  1  1  0  0 -1 -1  0  0 ...
+3rd digit -------> 2 zeros | 0  0  1  |  1  1  0  0  0 -1 -1 -1 ...
+4th digit -------> 3 zeros | 0  0  0  1 | 1  1 ... 
+5th digit -------> 4 zeros | 0  0  0  0  1 | 1 1 1 1 0 0 0 0 0 -1 -1 -1 -1 -1 ...
+
+(3M + 1)th ------> 3M zeros
+
+......................................................................
+
+1. By the time we get to index [5976463] all sums before that point will be ZEROs 
+   so we will ignore them.
+
+2. Calculate in reverse and reverse our answer again.
+
+'''
+skip = num(data[:7])
+d = (data * 10000)[skip:][::-1]
+
+for _ in range(100):
+  d = accumulate(d, lambda a, b: abs(a + b) % 10)
+
+print('Solution 2:', num(list(d)[::-1][:8]))
