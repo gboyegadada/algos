b72e27f50dfe13da1b864d23d98d8f5937cd8fba
Gboyega Dada <boyega@gmail.com>
2023-02-02 11:45:58 +0100

    AoC15 - Day 1

---
+++ AoC15/day1.py
@@ -0,0 +1,39 @@
+
+instructions = ''
+with open('day1_input.txt', 'r') as fp:
+
+  instructions = fp.readline()
+  
+# At what floor does Santa stop?
+def last_floor(l: str):
+  floor = 0
+  for x in l:
+    if x == '(':
+      floor += 1
+    else:
+      floor -= 1
+
+  return floor
+
+# At which instruction does Santa enter the 
+# basement for the first time?
+def enters_basement_at(l: str):
+  floor, steps = 0, 0
+  for x in l:
+    steps += 1
+    if x == '(':
+      floor += 1
+    else:
+      floor -= 1
+
+    if floor == -1:
+      break
+
+  return steps
+    
+print('------------ PART 01 -------------')
+print('Floor number: ', last_floor(instructions))
+
+
+print('------------ PART 02 -------------')
+print('Enters basement at: ', enters_basement_at(instructions))
