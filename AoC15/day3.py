64435d3237e0dbcb74d3f59cf6b2107f1d830ee6
Gboyega Dada <boyega@gmail.com>
2023-02-04 13:47:56 +0100

    AoC15 - Day 3

---
+++ AoC15/day3.py
@@ -0,0 +1,63 @@
+instructions = ''
+with open('day3_input.txt', 'r') as fp:
+  instructions = fp.readline()
+
+def santa_alone():
+  v = {(0, 0)} # ...visited
+  x, y = 0, 0 # ...Santa's last step
+
+  for step in instructions:
+    if '^' == step:
+      y += 1
+    elif 'v' == step:
+      y -= 1
+    elif '>' == step:
+      x += 1
+    elif '<' == step:
+      x -= 1
+    v.add((x, y))
+
+  return v
+
+def with_robo_santa():
+  v = {(0, 0)} # ...visited
+  x, y = 0, 0 # ...Santa's last step
+  xb, yb = 0, 0 # ...bots's last step
+  santa_is_next = True
+
+  for step in instructions:
+    if santa_is_next:
+      if '^' == step:
+        y += 1
+      elif 'v' == step:
+        y -= 1
+      elif '>' == step:
+        x += 1
+      elif '<' == step:
+        x -= 1
+      v.add((x, y))
+
+    else:
+      if '^' == step:
+        yb += 1
+      elif 'v' == step:
+        yb -= 1
+      elif '>' == step:
+        xb += 1
+      elif '<' == step:
+        xb -= 1
+      v.add((xb, yb))
+
+    santa_is_next = not santa_is_next
+
+  return v
+
+print('------------ PART 01 -------------')
+print('With Santa by himself,', len(santa_alone()), 'houses receive at least one present.')
+
+
+print('\n------------ PART 02 -------------')
+print('With Robo-Santa by his side,', len(with_robo_santa()), 'houses receive at least one present.')
+  
+
+
