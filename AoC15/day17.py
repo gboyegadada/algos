27386b0e9aeb0473935962a4836345fb991f6a30
Gboyega Dada <boyega@gmail.com>
2023-03-01 16:31:56 +0100

    AoC15 - Day 17 [clean up]

--- AoC15/day17.py
+++ AoC15/day17.py
@@ -5,18 +5,14 @@ from itertools import combinations
 with open('day17_input.txt', 'r') as f: 
   containers = [int(l) for l in f]
 
-def sum_perm(p: list, cl: list):
-    return 150 == sum([cl[i] for i in p])
-
-def calc_num_of_containers(data: list):
+def calc_num_of_ways(c: list):
   min_ways, max_ways = 0, 0
-  l = len(data)
+  l = len(c)
 
   # Generate all combinations of containers
   # ...and only select those volumes that add up to exactly 150l
-  # We will also filter out reverse connections i.e. a->b->c is the same as c->b->a
   for dl in range(1, l+1):
-    ways = [p for p in combinations(range(l), dl) if p <= p[::-1] and sum_perm(p, data)]
+    ways = [p for p in combinations(range(l), dl) if 150 == sum([c[i] for i in p])]
     lw = len(ways)
 
     if 0 == min_ways and lw > 0: min_ways = lw
@@ -25,7 +21,7 @@ def calc_num_of_containers(data: list):
 
   return max_ways, min_ways
 
-maxw, minw = calc_num_of_containers(containers)
+maxw, minw = calc_num_of_ways(containers)
 
 print('------------ PART 01 -------------')
 print('Combinations of containers that can exactly fit all 150 liters of eggnog:', maxw)
